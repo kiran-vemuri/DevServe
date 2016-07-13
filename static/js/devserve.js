faf95a08ccddafcdfc8ccd0ca91026b7268dfac5
Kiran Vemuri <kvemuri@adaranetworks.com>
2016-07-13 11:24:09 -0700

    - javascript in a separate file - javascript to confirm binary status changes

---
+++ static/js/devserve.js
@@ -0,0 +1,16 @@
+// Data Table
+$(document).ready(function() {
+    $('#sortedtable').DataTable({
+        "order": [[1, "desc"]],
+        "pageLength": 10,
+    });
+});
+
+
+
+
+// Binary status changes
+function status_change() {
+    var abc = confirm('Do you want to change the status on this item?');
+    return abc
+    }
